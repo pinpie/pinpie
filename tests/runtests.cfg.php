5406e504c7fbd46bd4f8d8d9303f84d7c2b60dbd
Igor Data <igor.s.data@gmail.com>
2017-01-18 22:19:24 -0800

    getting codecov.io bage

--- tests/runtests.cfg.php
+++ tests/runtests.cfg.php
@@ -7,3 +7,9 @@ $cloverWriter = new atoum\writers\file('coverage.xml');
 $cloverReport = new atoum\reports\asynchronous\clover();
 $cloverReport->addWriter($cloverWriter);
 $runner->addReport($cloverReport);
+
+$script->addDefaultReport();
+$telemetry = new mageekguy\atoum\reports\telemetry();
+$telemetry->addWriter(new mageekguy\atoum\writers\std\out());
+$telemetry->setProjectName('pinpie/pinpie');
+$runner->addReport($telemetry);
