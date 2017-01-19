af5878543b9f3775b87d09dbbe205e8b02dc9930
Igor Data <igor.s.data@gmail.com>
2017-01-18 22:52:06 -0800

    composer.lock removed

--- tests/runtests.cfg.php
+++ tests/runtests.cfg.php
@@ -3,13 +3,16 @@
 $runner->addTestsFromDirectory(__DIR__ . '/units');
 $script->addDefaultReport();
 
-$cloverWriter = new atoum\writers\file('coverage.xml');
-$cloverReport = new atoum\reports\asynchronous\clover();
-$cloverReport->addWriter($cloverWriter);
-$runner->addReport($cloverReport);
 
-$script->addDefaultReport();
-$telemetry = new mageekguy\atoum\reports\telemetry();
-$telemetry->addWriter(new mageekguy\atoum\writers\std\out());
-$telemetry->setProjectName('pinpie/pinpie');
-$runner->addReport($telemetry);
+if (PHP_MAJOR_VERSION === 5 AND PHP_MINOR_VERSION > 4) {
+	$cloverWriter = new atoum\writers\file('coverage.xml');
+	$cloverReport = new atoum\reports\asynchronous\clover();
+	$cloverReport->addWriter($cloverWriter);
+	$runner->addReport($cloverReport);
+
+	$script->addDefaultReport();
+	$telemetry = new mageekguy\atoum\reports\telemetry();
+	$telemetry->addWriter(new mageekguy\atoum\writers\std\out());
+	$telemetry->setProjectName('pinpie/pinpie');
+	$runner->addReport($telemetry);
+}
